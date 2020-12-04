fca0fa445ad62f2e97922531bee3c590f6ef7a40
soltan <soltandevn1@gmail.com>
2020-12-04 17:53:23 -0400

    added json and urlencoded middlewares from express and cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import express from "express";
-import controller from "./utils/base.controller"
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import controller from "./utils/base.controller";
 
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.get('/', controller().getAll)
 
